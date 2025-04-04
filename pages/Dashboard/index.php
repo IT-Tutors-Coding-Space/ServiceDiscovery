1f6d168f1c22ea497ca663e946209ed955351782
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-04-04 13:00:40 +0300

    feat: remove settings link from sidebar in dashboard

--- pages/Dashboard/index.php
+++ pages/Dashboard/index.php
@@ -33,7 +33,7 @@
                 
             </button>
             <div class="profile-dropdown" id="profile-dropdown">
-                <a href="#" data-page="profile" onclick="navigateTo('profile')" style="color:blue">👤 Profile</a>
+                <a href="#" data-page="profile" style="color:blue">👤 Profile</a>
                 <a href="#" onclick="event.preventDefault(); logout();" style="color:red">↻ Logout</a>
             </div>
         </div>
@@ -43,7 +43,7 @@
     <aside class="sidebar" id="sidebar">
         <ul>
             <li><a href="#" data-page="business_dashboard"><i class="fas fa-home"></i> <span class="text">Dashboard</span></a></li>
-            <li><a href="#" data-page="settings"><i class="fas fa-cog"></i> <span class="text">Settings</span></a></li>
+            <!-- <li><a href="#" data-page="settings"><i class="fas fa-cog"></i> <span class="text">Settings</span></a></li> -->
             <li><a href="#" data-page="manage_listings"><i class="fas fa-list"></i> <span class="text">Manage Listings</span></a></li>
             <li><a href="#" data-page="messages"><i class="fas fa-envelope"></i> <span class="text">Browse Requests</span></a></li>            
         </ul>
